37a77aea40c0ad7c5becfb901abb80be40a99858
guille15j <guillermosantosanchez@gmail.com>
2026-05-18 16:06:31 +0200

    Configuraciond e variables de entorno

--- src/api/apiService.js
+++ src/api/apiService.js
@@ -1,6 +1,6 @@
-const API_URL = process.env.EXPO_PUBLIC_API_URL;
-const X_API_KEY = process.env.EXPO_PUBLIC_X_API_KEY;
-const GOOGLE_CLIENT_ID = process.env.EXPO_PUBLIC_GOOGLE_CLIENT_ID;
+const API_URL = process.env.API_URL;
+const X_API_KEY = process.env.X_API_KEY;
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 
 import { authStorage } from '../core/authStorage';
 
